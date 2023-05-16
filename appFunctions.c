f5ed7df2a072ed9274f3b066678dfebfc16e5e1f
Buitragox <1264452-Buitragox@users.noreply.replit.com>
2023-05-16 03:48:35 +0000

    Add app state functions

---
+++ appFunctions.c
@@ -0,0 +1,86 @@
+#include "appFunctions.h"
+
+APP_STATES appIdle(msg_t *in_msg) {
+  APP_STATES next_state = IdleA;
+  msg_t out_msg; /* output message */
+
+  switch (in_msg->signal) {
+  case updateRule:
+    out_msg.signal = ruleUpdated;
+    out_msg.value_int = in_msg->value_int;
+    out_msg.value_float = in_msg->value_float;
+    sendMessage(&(queue[CLOUD_Q]), out_msg);
+    printf("\t--- App sent signal: ruleUpdated(%d, %f) TO Cloud\n",
+           out_msg.value_int, out_msg.value_float);
+    next_state = WaitConfirmUpdate;
+    break;
+
+  case appDecisionRequest:
+    out_msg.signal = userDecisionRequest;
+    /* Send message to user */
+    printf("\t--- App sent signal: userDecisionRequest\n");
+    printf("\t--- App waiting for USER INPUT\n");
+    next_state = WaitUserDecision;
+
+    // TODO: Set timer
+    break;
+
+  case consumptionReport:
+    out_msg.signal = showReport;
+    out_msg.value_float = in_msg->value_float;
+    /* Send message to user */
+    printf("\t--- App sent signal: showReport %f\n", out_msg.value_float);
+    next_state = IdleA;
+    break;
+
+  default:
+    break;
+  }
+
+  fflush(stdout);
+  return next_state;
+}
+
+APP_STATES appWaitConfirmUpdate(msg_t *in_msg) {
+  APP_STATES next_state = IdleA;
+  msg_t out_msg; /* output message */
+
+  switch (in_msg->signal) {
+  case confirmUpdate:
+    out_msg.signal = confirmRule;
+    out_msg.value_int = in_msg->value_int;
+    /* Send message to user */
+    printf("\t--- App sent signal: confirmRule %d\n", out_msg.value_int);
+    next_state = IdleA;
+    break;
+
+  default:
+    break;
+  }
+
+  fflush(stdout);
+  return next_state;
+}
+
+APP_STATES appWaitUserDecision(msg_t *in_msg) {
+  APP_STATES next_state = IdleA;
+  msg_t out_msg; /* output message */
+
+  switch (in_msg->signal) {
+  case userDecision:
+    out_msg.signal = appDecision;
+    out_msg.value_int = in_msg->value_int;
+    sendMessage(&(queue[CLOUD_Q]), out_msg);
+    printf("\t--- App sent signal: appDecision(%d) TO Cloud\n",
+           out_msg.value_int);
+    // printf("\t--- App sent signal: confirmRule %d\n", out_msg.value_int);
+    next_state = IdleA;
+    break;
+
+  default:
+    break;
+  }
+
+  fflush(stdout);
+  return next_state;
+}
