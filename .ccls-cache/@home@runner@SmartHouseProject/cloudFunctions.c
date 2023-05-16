0393352452dc96df308932ab670fa232b4c56c7c
Buitragox <1264452-Buitragox@users.noreply.replit.com>
2023-05-16 03:51:08 +0000

    Add cloud functions

---
+++ .ccls-cache/@home@runner@SmartHouseProject/cloudFunctions.c
@@ -0,0 +1,92 @@
+#include "cloudFunctions.h"
+
+CLOUD_STATES cloudIdle(msg_t *in_msg) {
+  CLOUD_STATES next_state = IdleCl;
+  msg_t out_msg; /* output message */
+
+  switch (in_msg->signal) {
+  case ruleUpdated:
+    out_msg.signal = changeParameter;
+    out_msg.value_int = in_msg->value_int;
+    out_msg.value_float = in_msg->value_float;
+    sendMessage(&(queue[CONTROLLER_Q]), out_msg);
+    printf("\t--- Cloud sent signal: changeParameter(%d, %f) TO Controller\n",
+           out_msg.value_int, out_msg.value_float);
+    next_state = WaitConfirmRule;
+    break;
+
+  case makeReport:
+    out_msg.signal = cloudConsumptionReq;
+    sendMessage(&(queue[CONTROLLER_Q]), out_msg);
+    printf("\t--- Cloud sent signal: cloudConsumptionReq TO Controller\n");
+    next_state = WaitReportCl;
+    break;
+
+  case decisionRequest:
+    out_msg.signal = appDecisionRequest;
+    sendMessage(&(queue[APP_Q]), out_msg);
+    printf("\t--- Cloud sent signal: appDecisionRequest TO App\n");
+    next_state = IdleCl;
+    break;
+
+  case appDecision:
+    out_msg.signal = cloudDecision;
+    out_msg.value_int = in_msg->value_int;
+    sendMessage(&(queue[CLOUD_Q]), out_msg);
+    printf("\t--- Cloud sent signal: appDecisionRequest(%d) TO Cloud\n",
+           out_msg.value_int);
+    next_state = IdleCl;
+    break;
+
+  default:
+    break;
+  }
+
+  fflush(stdout);
+  return next_state;
+}
+
+CLOUD_STATES cloudWaitConfirmRule(msg_t *in_msg) {
+  CLOUD_STATES next_state = IdleCl;
+  msg_t out_msg; /* output message */
+
+  switch (in_msg->signal) {
+  case confirmChange:
+    out_msg.signal = confirmUpdate;
+    out_msg.value_int = in_msg->value_int;
+    sendMessage(&(queue[APP_Q]), out_msg);
+    printf("\t--- Cloud sent signal: confirmUpdate(%d) TO App\n",
+           out_msg.value_int);
+    next_state = IdleCl;
+    break;
+
+  default:
+    break;
+  }
+
+  fflush(stdout);
+  return next_state;
+}
+
+CLOUD_STATES cloudWaitReport(msg_t *in_msg) {
+  CLOUD_STATES next_state = IdleCl;
+  msg_t out_msg; /* output message */
+
+  switch (in_msg->signal) {
+  case consumptionDevices:
+    out_msg.signal = consumptionReport;
+    out_msg.value_int = in_msg->value_int;
+    out_msg.value_float = in_msg->value_float;
+    sendMessage(&(queue[APP_Q]), out_msg);
+    printf("\t--- Cloud sent signal: consumptionReport(%f) TO App\n",
+           out_msg.value_float);
+    next_state = IdleCl;
+    break;
+
+  default:
+    break;
+  }
+
+  fflush(stdout);
+  return next_state;
+}
